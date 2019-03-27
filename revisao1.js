04fd0cd1c637c795deb5f732dedefb6b393df874
aschaves1976 <roots17@gmail.com>
2019-03-26 22:43:42 -0300

    ESNext - Revisão #01'

---
+++ revisao1.js
@@ -0,0 +1,25 @@
+// let e const
+{
+    var a = 1
+    let b = 2
+    console.log(b)
+}
+console.log(a)
+//console.log(b) - nao funciona pois a variavel foi criada com let que pertence a um escopo de bloco
+
+//Template String
+const produto = 'IPad'
+console.log(`${produto} é caro`)
+
+//Destructuring - tirar de dentro de uma estrutura algo
+const [l,e, ...tras] = 'Cod3r'
+console.log(l,e,tras)
+const [x, y] = [1,2]
+console.log(x, y)
+const [z,,w] = [1,2,3] // nete caso estamos ignorando o elemento 2
+console.log(z, w)
+
+const{nome, idade} = {nome: 'Ana', idade: 9}
+console.log (nome, idade)
+const{prod: p, preco} = {preco: 300.7, prod: 'SSD'}
+console.log(p, preco)
